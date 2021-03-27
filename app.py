120051fe3761afa0dea0739985b8775ddf1aa94d
0kalekale <kalekale.anon@gmail.com>
2021-03-27 19:59:36 +0545

    added basic http get

--- app.py
+++ app.py
@@ -4,6 +4,16 @@ app = Flask(__name__, template_folder='templates')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return "werks"
+    db = open("data/db.txt", "rb").read()
+    return db
 
+@app.route('/msg', methods=['GET', 'POST'])
+def msg():
+    msg = request.args.get('msg', default = '', type = str)
+    uname = request.args.get('uname', default = '', type = str)
+    db = open("data/db.txt", "a")
+    string = uname + ": "+ msg + "\n"
+    db.write(string)
+    db.close
+    return "message sent!\n"
 app.run()
