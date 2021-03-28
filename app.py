37654930e7df532d7b508a209428ebcfe00e2736
0kalekale <kalekale.anon@gmail.com>
2021-03-28 20:08:27 +0545

    added channels

--- app.py
+++ app.py
@@ -4,16 +4,29 @@ app = Flask(__name__, template_folder='templates')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    db = open("data/db.txt", "rb").read()
-    return db
+   
+    return "index\n work in progress\n" 
 
 @app.route('/msg', methods=['GET', 'POST'])
 def msg():
-    msg = request.args.get('msg', default = '', type = str)
-    uname = request.args.get('uname', default = '', type = str)
-    db = open("data/db.txt", "a")
-    string = uname + ": "+ msg + "\n"
-    db.write(string)
-    db.close
-    return "message sent!\n"
+    msg = request.args.get('msg', default = 'NULL', type = str)
+    uname = request.args.get('uname', default = 'Anon', type = str)
+    channel = request.args.get('channel', default = '', type = str)
+
+    if channel == '':
+        return "mention the channel please!\n"
+    else:
+        filename = "data/" + "db-" + channel + ".txt"  
+        db = open(filename, "a")
+        string = uname + ": "+ msg + "\n"
+        db.write(string)
+        db.close
+        return "message sent!\n"
+
+@app.route('/channel/<name>', methods=['GET', 'POST'])
+def chan(name):
+    filename = "data/db-" + name + ".txt"
+    db = open(filename, "r")
+    return db.read()
+
 app.run()
